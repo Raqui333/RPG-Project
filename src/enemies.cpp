0727c47a1226a451adc60b6103f48ac040f9d9ec
UserUnavailable <klebersilva2025@gmail.com>
2019-03-09 18:30:32 -0300

    moved entity Enemies declarations to here

--- src/enemies.cpp
+++ src/enemies.cpp
@@ -1,10 +1,30 @@
 #include "../include/enemies.h"
 
+rpg::Enemies::Enemies(std::string x, int y, int z) {
+	name_t = x;
+	hp_t = std::make_pair(y, y);
+	mp_t = std::make_pair(z, z);
+}
+
+std::string rpg::Enemies::name() { return name_t; }
+int rpg::Enemies::dmg_rate() { return hp_t.second / 10; }
+int rpg::Enemies::xp() { return hp_t.second / 2; }
+int rpg::Enemies::money() { return hp_t.second / 10; }
+
+// hp
+int rpg::Enemies::hp() { return hp_t.first <= 0 ? 0 : hp_t.first; }
+void rpg::Enemies::hp(int new_hp) { hp_t.first = new_hp; }
+
+// mp
+int rpg::Enemies::mp() { return mp_t.first <= 0 ? 0 : mp_t.first; }
+void rpg::Enemies::mp(int new_mp) { mp_t.first = new_mp; }
+
+// set memebers
 rpg::Enemies rpg::raqui333("??????", 999999, 999999);
 
 typedef rpg::Enemies add;
 
-add rpg::goblin("Goblin", 50  , 50);
+add rpg::goblin("Goblin", 50, 50);
 add rpg::wolf("Wolf", 100, 10);
 add rpg::skeleton("Skeleton", 150, 150);
 add rpg::vampire("Vampire", 300, 200);
